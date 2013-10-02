25bbd0e4e43765dc8dbb95defa7f26d4ae6304e8
GabTheMad <allapierr@gmail.com>
2013-10-02 18:55:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-// Add your JavaScript below!
 $(document).ready(function(){
-    $("#bt").click(function(){
-        var login = document.getElementById("login").value;
-        var senha = document.getElementById("senha").value;
-        function validate(){
-            if(login.length>23){
-                alert("Nome muito grande");
-            }else if(login==null||login==""){
-                alert("Informe seu nome");
-            }else if(senha.length>23){
-                alert("Senha muito grande");
-            }else if(senha==null||senha==""){
-                alert("Informe sua senha");
-            }else{
-                document.write("<div>Bem vindo, "+login+"</div>");
-            }
-        }
-        validate();
-    })
+	//array com vários pusheens
+	var pusheens = ["http://cats-pictures.org/pictures/get_image/1106-1265x949-pusheen-solo-whiskers-grey+hair-standing-tail.png","http://static.tumblr.com/f222782a7821639aac0e3dc09718594f/iujc8q4/sowmlktoz/tumblr_static_catw.png","http://th05.deviantart.net/fs70/200H/f/2013/147/b/2/pusheen_the_cat_png_21_by_13taylorswiftlover13-d66qzf9.png","http://3.bp.blogspot.com/-RCy7QaSd9OY/T_XZfJ7g1JI/AAAAAAAAA9s/wEAQ8xJY-_s/s1600/Fatos+Inocentes+(4).png","http://4.bp.blogspot.com/--a0CJ4upXfQ/T_XZpnWr61I/AAAAAAAAA-U/kwpwQYa_ejc/s1600/Fatos+Inocentes+(11).png"];
+	//pegando um pusheen aleatório e jogando na div
+	//$("#pusheenimg").attr("src",pusheens[Math.floor(Math.random()*pusheens.length)]);
+
 })
